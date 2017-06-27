288dd34451b9f88f3566e4266a1a2530586175b3
MilesCranmer <miles.cranmer@gmail.com>
2017-06-27 10:53:05 +1000

    test_exe_mode.sh: Clean up assert function

--- test/test_exe_mode.sh
+++ test/test_exe_mode.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 # From http://tldp.org/LDP/abs/html/debugging.html
-assert ()                 #  If condition false,
-{                         #+ exit from script
-    #+ with appropriate error message.
+assert ()
+{
     E_PARAM_ERR=98
     E_ASSERT_FAILED=99
-
-
-    if [ -z "$2" ]          #  Not enough parameters passed
-    then                    #+ to assert() function.
-        return $E_PARAM_ERR   #  No damage done.
-    fi
-
-    lineno=$2
-
-    if [ ! $1 ] 
+    if [ -z "$2" ]
     then
-        echo "Test assertion failed:  \"$1\""
-        echo "Test \"$0\", line $lineno"    # Give name of file and line number.
+        return $E_PARAM_ERR
+    fi
+    POINTED_LINE_NO=$2
+    if [ ! $1 ]
+    then
+        echo -n "Test assertion failed:  \"$1\","
+        echo "Test \"$0\", line $POINTED_LINE_NO"
         exit $E_ASSERT_FAILED
-        # else
-        #   return
-        #   and continue executing the script.
-    fi  
-} # Insert a similar assert() function into a script you need to debug.    
+    fi
+}
 
 cat test_files/python.py | ../vims -e '^\s\+def __init__' 'V/^\\s\\+def\<enter>kdGp' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_init_at_bottom.py)
