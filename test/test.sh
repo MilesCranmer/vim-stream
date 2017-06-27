0902dbf26f939239e9ae57491e10c96375b07a8a
MilesCranmer <miles.cranmer@gmail.com>
2017-06-27 10:41:14 +0000

    test.sh: Fix assert statement
    - Did not quote the args, so the diff was getting passed as multiple
      arguments

--- test/test.sh
+++ test/test.sh
@@ -9,10 +9,11 @@ assert ()
         return $E_PARAM_ERR
     fi
     POINTED_LINE_NO=$2
-    if [ ! $1 ]
+    if [ ! -z "$1" ]
     then
-        echo -n "Test assertion failed:  \"$1\","
-        echo "Test \"$0\", line $POINTED_LINE_NO"
+        echo -n "- test assertion failed!"
+        echo -n " test $3, line $POINTED_LINE_NO, "
+        echo " diff: $1"
         exit $E_ASSERT_FAILED
     fi
 }
@@ -20,11 +21,13 @@ assert ()
 cat test_files/python.py | ../vims -e '^\s\+def __init__' 'V/^\\s\\+def\<enter>kdGp' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_init_at_bottom.py)
 rm .tmp
-assert [ $DIFF == "" ] $LINENO 
+assert "$DIFF" $LINENO 0
 echo "+ test to move init to bottom passed"
+
 cat test_files/python.py | ../vims -n '$-3,$p' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_last_4_lines.py)
 rm .tmp
-assert [ $DIFF == "" ] $LINENO 
+assert "$DIFF" $LINENO 1
 echo "+ test to print last 4 lines passed"
+
 echo "+ tests all passed"
