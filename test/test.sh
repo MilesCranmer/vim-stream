0db143356dd7a8aa8a13b33a5a23fb5eaa5bfdc5
MilesCranmer <miles.cranmer@gmail.com>
2017-06-27 17:05:19 +0000

    Print number on each passed test

--- test/test.sh
+++ test/test.sh
@@ -21,43 +21,51 @@ assert ()
 
 cat test_files/python.py | ../vims '' > .tmp
 DIFF=$(diff -b .tmp test_files/python.py)
-assert "$DIFF" $LINENO 0
-echo "+ test to do a non-edit passed"
+TEST_NUM=0
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to do a non-edit passed"
 
 cat test_files/python.py | ../vims -e '^\s\+def __init__' 'V/^\\s\\+def\<enter>kdGp' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_init_at_bottom.py)
-assert "$DIFF" $LINENO 1
-echo "+ test to move init to bottom passed"
+TEST_NUM=1
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to move init to bottom passed"
 
 cat test_files/python.py | ../vims -n '$-3,$p' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_last_4_lines.py)
-assert "$DIFF" $LINENO 2
-echo "+ test to print last 4 lines passed"
+TEST_NUM=2
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to print last 4 lines passed"
 
 cat test_files/python.py | ../vims -s '/^class\<enter>O# This class is for Bifrost\<esc>Go\<enter># This file does not run!' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_with_extra_comments.py)
-assert "$DIFF" $LINENO 3
-echo "+ test to do one long 'simple' command passed"
+TEST_NUM=3
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to do one long 'simple' command passed"
 
 cat test_files/python.py | ../vims -s 'x' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_minus_one_char.py)
-assert "$DIFF" $LINENO 3
-echo "+ test to do one short 'simple' command passed"
+TEST_NUM=3
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to do one short 'simple' command passed"
 
 cat test_files/python.py | ../vims -e '.*' ':m0\<enter>' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_reversed.py)
-assert "$DIFF" $LINENO 4
-echo "+ test to reverse a file with exe passed"
+TEST_NUM=4
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to reverse a file with exe passed"
 
 cat test_files/python.py | ../vims '%g/.*/m0' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_reversed.py)
-assert "$DIFF" $LINENO 5
-echo "+ test to reverse a file in normal mode passed"
+TEST_NUM=5
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to reverse a file in normal mode passed"
 
 cat test_files/numbers.txt | ../vims -e '^5$' 'dd' -t '%g/^3$/t$' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/numbers_5_gone_3_bottom.txt)
-assert "$DIFF" $LINENO 6
-echo "+ test to turn back off exe mode"
+TEST_NUM=6
+assert "$DIFF" $LINENO $TEST_NUM
+echo "+ test $TEST_NUM to turn back off exe mode"
 
 echo "+ tests all passed"
 rm .tmp
