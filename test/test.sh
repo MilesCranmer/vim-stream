d2737a028140decd016f97e4896d2d9908e6b29f
MilesCranmer <miles.cranmer@gmail.com>
2017-06-27 11:20:19 +0000

    test.sh: Only rm tmp file if tests good

--- test/test.sh
+++ test/test.sh
@@ -21,14 +21,13 @@ assert ()
 
 cat test_files/python.py | ../vims -e '^\s\+def __init__' 'V/^\\s\\+def\<enter>kdGp' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_init_at_bottom.py)
-rm .tmp
 assert "$DIFF" $LINENO 0
 echo "+ test to move init to bottom passed"
 
 cat test_files/python.py | ../vims -n '$-3,$p' | cat > .tmp
 DIFF=$(diff -b .tmp test_files/python_last_4_lines.py)
-rm .tmp
 assert "$DIFF" $LINENO 1
 echo "+ test to print last 4 lines passed"
 
 echo "+ tests all passed"
+rm .tmp
